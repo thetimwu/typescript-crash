75668cb0f71f9a55537f2b4fdbd2513fc450e7ad
tim wu <timwuuk@gmail.com>
2020-07-13 18:48:23 +0100

    add react context with useReducer etc...

--- src/App.tsx
+++ src/App.tsx
@@ -1,61 +1,48 @@
-import React, { useState, Fragment } from "react";
+import React, { useState, Fragment, useContext, useEffect } from "react";
 import "./App.css";
+import { VideoContext, IAction } from "./contexts/VideoContext";
 
 function App(): JSX.Element {
-  const [inputText, setinputText] = useState<string>("");
+  const { state, dispatch } = useContext(VideoContext);
 
-  interface ITodo {
-    text: string;
-    complete: boolean;
+  useEffect(() => {
+    state.episodes.length === 0 && fetchData();
+  });
+
+  interface Post {
+    userId: number;
+    id: number;
+    title: string;
+    body: string;
   }
-  const [todos, setTodos] = useState<ITodo[]>([]);
 
-  const inputHandler = (e: React.ChangeEvent<HTMLInputElement>): void => {
-    setinputText(e.target.value);
-    // console.log(inputText);
+  const fetchData = async () => {
+    const url = "https://jsonplaceholder.typicode.com/posts";
+    const data = await fetch(url);
+    const dataJson = await data.json();
+    return dispatch({
+      type: "FETCH_DATA",
+      payload: dataJson,
+    });
   };
 
-  // type FormElem = React.FormEvent<HTMLFormElement>;
-  type ButtomElem = React.MouseEvent<HTMLButtonElement, MouseEvent>;
-
-  const addHandler = (e: ButtomElem): void => {
-    e.preventDefault();
-    setTodos([...todos, { text: inputText, complete: false }]);
-    setinputText("");
-  };
-
-  const toggleHandler = (i: number): void => {
-    const newTodos = [...todos];
-    newTodos[i].complete = !newTodos[i].complete;
-    setTodos(newTodos);
-  };
-
-  const deleteHandler = (i: number): void => {
-    const newTodos = todos.filter((item, index) => index !== i);
-    setTodos(newTodos);
-  };
+  const addFavorite = (post: Post): IAction =>
+    dispatch({
+      type: "ADD_FAVORITE",
+      payload: post,
+    });
 
   return (
-    <div className="App">
-      <h1>Todo list App</h1>
-      <form action="">
-        <input type="text" value={inputText} onChange={inputHandler} />
-        <button onClick={addHandler}>Add</button>
-      </form>
-      <div>
-        {todos.map((item, i) => (
-          <Fragment key={i}>
-            <div
-              onClick={() => deleteHandler(i)}
-              style={{ textDecoration: item.complete ? "line-through" : "" }}
-            >
-              {item.text}
-            </div>
-            <button onClick={() => toggleHandler(i)}>Toggle</button>
-          </Fragment>
-        ))}
-      </div>
-    </div>
+    <section>
+      {state.episodes.map((item: Post) => (
+        <section key={item.id}>
+          <div>{item.title}</div>
+          <p>{item.body}</p>
+          <button onClick={() => addFavorite(item)}>Add Favorite</button>
+        </section>
+      ))}
+      {console.log(state.favourites)}
+    </section>
   );
 }
 
