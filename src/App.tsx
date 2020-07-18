5e22cf7ebaee623452796e279e2e185514604afd
tim wu <timwuuk@gmail.com>
2020-07-18 11:45:21 +0100

    useRef

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,10 @@
-import React, { useState, Fragment, useContext, useEffect } from "react";
+import React, {
+  useState,
+  Fragment,
+  useContext,
+  useEffect,
+  useRef,
+} from "react";
 import "./App.css";
 import { useForm } from "./useForm";
 import useFetch from "./useFetch";
@@ -12,23 +18,25 @@ function App(): JSX.Element {
   const [values, setValue] = useForm({ name: "", password: "" });
   const [num, setNum] = useState<number>(1);
   const { data, loading } = useFetch(`http://numbersapi.com/${num}/trivia`);
+  const inputText = useRef<HTMLInputElement>(null!);
+  const inputNum = useRef<number>(0);
 
-  useEffect(() => {
-    const onMouseMove = (e: MouseEvent) => {
-      console.log(e);
-    };
-    window.addEventListener("mousemove", onMouseMove);
-    return () => {
-      window.removeEventListener("mousemove", onMouseMove);
-    };
-  }, []);
+  // useEffect(() => {
+  //   const onMouseMove = (e: MouseEvent) => {
+  //     console.log(e);
+  //   };
+  //   window.addEventListener("mousemove", onMouseMove);
+  //   return () => {
+  //     window.removeEventListener("mousemove", onMouseMove);
+  //   };
+  // }, []);
 
   return (
     <div>
       <div>{!data ? "Loading..." : data}</div>
       <button onClick={() => setNum(num + 1)}>Add 1</button>
       <form action="">
-        <input type="text" name="name" onChange={setValue} />
+        <input ref={inputText} type="text" name="name" onChange={setValue} />
         <input
           type="password"
           name="password"
@@ -38,6 +46,11 @@ function App(): JSX.Element {
       </form>
       <div>{values.password}</div>
       <div>{values.name}</div>
+      <div>
+        <button onClick={() => console.log(inputNum.current++)}>
+          Test useRef
+        </button>
+      </div>
     </div>
   );
 }
